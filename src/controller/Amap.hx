fe4339b0e259c590ffbcac4a718c0abd6b37f789
Corentin <corentincloarec@hotmail.fr>
2023-03-23 11:12:45 +0100

    mode marché

--- src/controller/Amap.hx
+++ src/controller/Amap.hx
@@ -43,12 +43,6 @@ class Amap extends Controller
 		var flags = form.getElement("flags");
 		untyped flags.excluded = [0,1,2,3,5,9];
 
-		//group mode
-		var mode ="Mode Marché";
-		var html = new sugoi.form.elements.Html("mode",mode,"Mode de commande");
-		html.docLink = "https://wiki.cagette.net/admin:admin_boutique";
-		form.addElement(html ,7);
-	
 		//payment help
 		var html = new sugoi.form.elements.Html("payments","<p class='desc'><a href='https://formation.alilo.fr/mod/page/view.php?id=821' target='_blank'><i class=\"icon icon-info\"></i> En savoir plus sur la gestion des paiements</a></p>","");
 		form.addElement(html ,9);
