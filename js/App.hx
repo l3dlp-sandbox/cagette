26ae04f5150c86cc64279918014708565982a1c2
Corentin <corentincloarec@hotmail.fr>
2023-03-23 10:41:31 +0100

    move function that creates a neo module from Frontend to Backend. Remove React npm.

--- js/App.hx
+++ js/App.hx
@@ -1,22 +1,14 @@
 import bootstrap.Modal;
 import js.Browser;
-import Common;
-// import thx.semver.Version;
 
-//React lib
-import react.ReactDOM;
 class App {
 
     public static var instance : App;    
     // public static var VERSION = ([0,14]  : Version).withPre(GitMacros.getGitShortSHA(), GitMacros.getGitCommitDate());
     	
 	public var currency : String; //currency symbol like &euro; or $
-	public var t : sugoi.i18n.GetText;//gettext translator
 	public var Modal = bootstrap.Modal;
     public var lang : String;
-    public var userId : Int;
-    public var userName : String;
-    public var userEmail : String;
     
 
 	function new(?lang="fr",?currency="&euro;") {
@@ -26,113 +18,15 @@ class App {
 		this.currency = currency;
 	}
 
-	/**
-	**/
-	/*public static inline function jq(r:Dynamic):js.jquery.JQuery{
-		trace("CALL JQUERY");
-		return new js.jquery.JQuery(r);
-	}*/
 
 	/**
 	 * The JS App will be available as "_" in the document.
 	 */
 	public static function main() {
         var app = new App();
-        untyped js.Browser.window._ = app;
         untyped js.Browser.window._Cagette = app;//avoid conflicts with lodash
     }
     
-
-	// public function getTuto(name:String, step:Int) {
-    
-	// 	new Tuto(name,step);
-	// }
-	
-	/**
-	 * remove method for IE compat
-	 */
-	public function remove(el:js.html.Element){
-		if (el == null) return;
-		el.parentElement.removeChild(el);
-	}
-	
-	public function getVATBox(ttcprice:Float,currency:String,rates:String,vat:Float,formName:String){
-        js.Browser.document.addEventListener("DOMContentLoaded", function(event) {
-            
-            var input = js.Browser.document.querySelector('form input[name="${formName}_price"]');
-            remove( js.Browser.document.querySelector('form input[name="${formName}_vat"]').parentElement.parentElement );		
-            
-            input.parentElement.id = "vat-box-neo-container";
-
-            var neo:Dynamic = Reflect.field(js.Browser.window, 'neo');
-            neo.createNeoModule(input.parentElement.id, "vatBox", {
-                initialTtc: ttcprice,
-                currency: currency,
-                vatRates: rates,
-                initialVat: vat,
-                formName: formName
-            });
-        });
-    }
-
-	/**
-	 * Removes the form element and replace it by a react js component
-	 * @param	divId
-	 * @param	productName
-	 * @param	txpProductId
-	 * @param	formName
-	 */
-	public function getProductInput(divId:String, productName:String, txpProductId:Null<Int>, formName:String ){
-		js.Browser.document.addEventListener("DOMContentLoaded", function(event) {
-
-			//dirty stuff to remove "real" input, and replace it by the react one
-			remove(Browser.document.querySelector("form input[name='"+formName+"_name']").parentElement.parentElement);
-			remove(Browser.document.querySelector("form select[name='" + formName+"_txpProductId']").parentElement.parentElement);
-
-            var neo:Dynamic = Reflect.field(js.Browser.window, 'neo');
-            neo.createNeoModule(divId, "productCategorizer", {
-                originalProductName: productName,
-                originalTxpProductId: txpProductId,
-                formName: formName
-            });
-		});
-	}
-
-	public function getVendorPage(divId:String, vendorId:Int, ?catalogId:Int ) {
-		js.Browser.document.addEventListener("DOMContentLoaded", function(event) {
-            var neo:Dynamic = Reflect.field(js.Browser.window, 'neo');
-            neo.createNeoModule(divId, "vendorPublicPage", {
-                vendorId: vendorId,
-                pCatalogId: catalogId
-            });
-		});
-	}
-
-	public function openImageUploader( imageUploaderContext : String, entityId: Int, width:Int, height:Int, ?imageType: String, ?currentCagetteProId: Int ) {
-		var nodeId = "image-uploader-container";
-        var node = js.Browser.document.getElementById(nodeId);
-        if (node == null) {
-            node = js.Browser.document.createDivElement();
-            node.id = nodeId;
-            js.Browser.document.body.appendChild(node);
-        }
-
-        var neo:Dynamic = Reflect.field(js.Browser.window, 'neo');
-        neo.createNeoModule(node.id, "imageUploaderDialog", {
-            context: imageUploaderContext,
-            entityId: entityId,
-            width: width,
-            height: height,
-            imageType: imageType,
-            currentCagetteProId: currentCagetteProId
-        });
-	}
-	
-	public static function roundTo(n:Float, r:Int):Float {
-		return Math.round(n * Math.pow(10,r)) / Math.pow(10,r) ;
-	}
-
-
 	/**
 	 * Ajax loads a page and display it in a modal window
 	 * @param	url
@@ -154,103 +48,6 @@ class App {
 		r.request();
 	}
 
-    public function modal(divId:String) {		
-
-			var modalElement = Browser.document.getElementById(divId);
-			var modal = new Modal(modalElement);
-			// modalElement.querySelector(".modal-body").innerHTML = data;
-			// if (title != null) modalElement.querySelector(".modal-title").innerHTML = title;
-			// if (!large) modalElement.querySelector(".modal-dialog").classList.remove("modal-lg");
-			modal.show();
-	}
-
-	/**
-	 * Displays a login box
-	 */
-	public function loginBox(redirectUrl:String,sid:String,?message:String,?phoneRequired=false,?addressRequired=false,?openRegistration=false,?invitedUserEmail:String, ?invitedGroupId:Int, ?asDialog:Bool) {	
-        var node = js.Browser.document.createDivElement();
-		node.id = "login-registration-container";
-		js.Browser.document.body.appendChild(node);
-		ReactDOM.unmountComponentAtNode(node); //the previous modal DOM element is still there, so we need to destroy it
-        
-        var neo:Dynamic = Reflect.field(js.Browser.window, 'neo');
-        neo.createNeoModule(node.id, "loginRegistration", {
-            redirectUrl: redirectUrl,
-            sid: sid,
-            message: message,
-            phoneRequired: phoneRequired,
-            addressRequired: addressRequired,
-            openRegistration: openRegistration,
-            invitedUserEmail: invitedUserEmail,
-            invitedGroupId: invitedGroupId,
-            asDialog: asDialog
-        });
-	}
-
-	/**
-	 *  Displays a sign up box
-	 */
-	public function registerBox(redirectUrl:String,sid:String,?message:String,?phoneRequired=false,?addressRequired=false, ?invitedUserEmail:String, ?invitedGroupId:Int) {
-        loginBox(redirectUrl, sid, message, phoneRequired, addressRequired, true, invitedUserEmail, invitedGroupId);
-	}
-
-	public function membershipBox(userId:Int,userName:String,groupId:Int,?callbackUrl:String,?distributionId:Int){
-		var node = js.Browser.document.createDivElement();
-		node.id = "membershipBox-container";
-		js.Browser.document.body.appendChild(node);
-		ReactDOM.unmountComponentAtNode(node); //the previous modal DOM element is still there, so we need to destroy it
-    
-        var neo:Dynamic = Reflect.field(js.Browser.window, 'neo');
-        neo.createNeoModule(node.id, "membershipDialog", {
-            groupId: groupId,
-            userId: userId,
-            userName: userName,
-            callbackUrl: callbackUrl,
-            distributionId: distributionId
-        });
-	}
-
-	/**
-	 * Helper to get values of a bunch of checked checkboxes
-	 * @param	formSelector
-	 */
-	public function getCheckboxesId(formSelector:String):Array<String>{
-		var out = [];
-		var checkboxes = js.Browser.document.querySelectorAll(formSelector + " input[type=checkbox]");
-		for ( input in checkboxes ){
-			var input : js.html.InputElement = cast input;
-			if ( input.checked ) out.push(input.value);
-		}
-		return out;
-	}
-
-
-	#if plugins
-	/*public function getHostedPlugin(){
-		return new hosted.js.App();
-	}*/
-	#end
-
-	/**
-	 * set up a warning message when leaving the page
-	 */
-	public function setWarningOnUnload(active:Bool, ?msg:String){
-		if (active){
-			js.Browser.window.addEventListener("beforeunload", warn);
-		}else{
-			js.Browser.window.removeEventListener("beforeunload", warn);
-		}
-
-	}
-
-	function warn(e:js.html.Event) {
-		var msg = "Voulez vous vraiment quitter cette page ?";
-		//js.Browser.window.confirm(msg);
-		untyped e.returnValue = msg; //Gecko + IE
-		e.preventDefault();
-		return msg; //Gecko + Webkit, Safari, Chrome etc.
-	}
-
 	/**
 	 * Anti Doubleclick with btn elements.
 	 * Can be bypassed by adding a .btn-noAntiDoubleClick class
@@ -269,70 +66,9 @@ class App {
 		
 	}
 
-	/**
-		Anti doubleclick link function
-	**/
-	public static var linkClicked = false;
-	public function goto(url:String){
-		
-		if(!linkClicked) {
-			js.Browser.document.location.href=url;
-			linkClicked = true;
-		}else{
-			js.Browser.console.log("double click detected");
-		}
-	}
-
-	/**
-		Display a notif about new features 3 times
-	**/
-	/*public function newFeature(selector:String,title:String,message:String,placement:String){
-		var element = js.Browser.document.querySelector(selector);
-		if(element==null) return;
-
-		//do not show after 3 times
-		var storage = js.Browser.getLocalStorage();
-		if (storage.getItem("newFeature."+selector) == "3") return;
-
-		//prepare Bootstrap "popover"
-		var x = jq(element).first().attr("title",title);
-		var text = "<p>" + message + "</p>";
-		
-		var options = { container:"body", content:text, html:true , placement:placement};
-		untyped  x.popover(options).popover('show');
-		//click anywhere to hide
-		App.jq("html").click(function(_) {
-			untyped x.popover('hide');				
-		});
-		
-
-		var storage = js.Browser.getLocalStorage();
-		var i = storage.getItem("newFeature."+selector);
-		if(i==null) i = "0";
-		storage.setItem("newFeature."+selector, Std.string( Std.parseInt(i)+1 ) );
-		
-		//highlight
-		App.jq(element).first().addClass("highlight");	
-	}*/
-
-	public function toggle(selector:String){
-		for ( el in js.Browser.document.querySelectorAll(selector)){
-			untyped el.classList.toggle("hidden");
-		}
-	}
-
-	public function show(selector:String){
-		for ( el in js.Browser.document.querySelectorAll(selector)){
-			untyped el.classList.remove("hidden");
-		}
-	}
-
-	public function hide(selector:String){
-		for ( el in js.Browser.document.querySelectorAll(selector)){
-			untyped el.classList.add("hidden");
-		}
-	}
-
+    /**
+	 * Used in TS code after leaving a group
+	 */
     public function resetGroupInSession(groupId:Int) {
         var req = new haxe.Http("/account/quitGroup/"+groupId);
         req.request();
