daf47a3a553a5a09472a72bee861b168780ad4da
Corentin <corentincloarec@hotmail.fr>
2023-03-28 14:32:13 +0200

    reset doProductInfo

--- src/controller/Shop.hx
+++ src/controller/Shop.hx
@@ -41,4 +41,15 @@ class Shop extends Controller
 		view.basket = basket;
 	} 
 
+	/**
+	 * Product infos popup used in many places
+	*/
+	@tpl('shop/productInfo.mtt')
+	public function doProductInfo(p:db.Product,?args:{distribution:db.Distribution}) {
+		var d = args!=null && args.distribution!=null ? args.distribution : null;
+		view.p = p.infos(null,null,d);
+		view.product = p;
+		view.vendor = p.catalog.vendor;
+	} 
+
 }
