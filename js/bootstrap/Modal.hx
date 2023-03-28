94e56e746a00d8a4d3c93fde243b08832cc8234a
Corentin <corentincloarec@hotmail.fr>
2023-03-28 15:46:09 +0200

    get rid of frontend

--- js/bootstrap/Modal.hx
+++
@@ -1,9 +0,0 @@
-package bootstrap;
-
-@:jsRequire('bootstrap.native', "Modal")  
-extern class Modal {
-  public function new (el: js.html.Element);
-  public function show(): Void;
-  public function hide(): Void;
-  public function addEventListener(event: String, callback: ()->Void): Void;
-}
